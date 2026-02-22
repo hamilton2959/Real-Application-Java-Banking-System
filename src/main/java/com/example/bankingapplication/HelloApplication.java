a91f8a9ba8c47b337c2ca613b9add44c5791d85b
Hamilton Migwambo <skytech2959@gmail.com>
2026-02-22 16:06:09 +0300

    init commit

---
+++ src/main/java/com/example/bankingapplication/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.bankingapplication;
+
+import com.example.bankingapplication.config.DatabaseConfig;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Initialize database
+        DatabaseConfig.initializeDatabase();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module com.example.bankingapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.sql;
+    requires java.prefs;
+    requires jbcrypt;
+
+    opens com.example.bankingapplication to javafx.fxml;
+    exports com.example.bankingapplication;
+    exports com.example.bankingapplication.controllers;
+    opens com.example.bankingapplication.controllers to javafx.fxml;
+    exports com.example.bankingapplication.config;
+    opens com.example.bankingapplication.config to javafx.fxml;
+}
